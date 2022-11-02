5ef6c85692683845952c37e4b2edc8aa84b8e328
Dagwbl <39078825+Dagwbl@users.noreply.github.com>
2022-11-02 22:36:48 +0800

    feat:完成sensor界面联调

--- src/components/sensor.vue
+++ src/components/sensor.vue
@@ -9,22 +9,24 @@
         <el-button type="danger" @click="handleDelMulti" v-if="multipleSelection.length>0">删除多选</el-button>
       </div>
     </div>
-    <el-table     ref="multipleTableRef"
-                  :data="tableData"
-                  :header-cell-style="{'text-align':'center'}"
-                  :cell-style="{'text-align':'center'}"
-                  style="width: 100%;"
-                  @selection-change="handleSelectionChange"
-                  border>
+    <el-table ref="multipleTableRef"
+              :data="tableData"
+              :header-cell-style="{'text-align':'center'}"
+              :cell-style="{'text-align':'center'}"
+              style="width: 100%;"
+              @selection-change="handleSelectionChange"
+              border>
       <el-table-column fixed="left" type="selection" label="Select"/>
-      <el-table-column prop="id" label="ID" width="120" />
-      <el-table-column prop="coords" label="Coords" />
-      <el-table-column prop="optocouple" label="Optocouple"  />
-      <el-table-column prop="response_level" label="Response Level" />
-      <el-table-column fixed="right" label="Operations" >
+
+      <template v-for="(item,index) in tableHead">
+        <el-table-column :prop="item.prop" :label="item.label" :key="index" v-if="item.prop != 'id'"></el-table-column>
+      </template>
+
+      <el-table-column fixed="right" label="Operations">
         <template #default="scope">
           <el-button link type="primary" size="small" @click="handleRowDelete(scope.row)" style="color: #F56C6C"
-          >Delete</el-button
+          >Delete
+          </el-button
           >
           <el-button link type="primary" size="small" @click="handleEdit(scope.row)">Edit</el-button>
         </template>
@@ -38,20 +40,36 @@
         class="mt-4"
         v-model:current-page="curPage"
         v-model:page-size="pageSize"
-        @current-change = "handleChangePage"
+        @current-change="handleChangePage"
         style="display: flex;justify-content: center;margin-top: 10px"
     />
     <el-dialog v-model="dialogFormVisible" :title="dialogType === 'add' ? 'Insert' : 'Edit'">
       <el-form :model="dialogForm">
-        <el-form-item label="Coords" :label-width="120">
-          <el-input v-model="dialogForm.coords" autocomplete="off" />
+        <el-form-item label="id" :label-width="120">
+          <el-input v-model="dialogForm.id" autocomplete="off"/>
+        </el-form-item>
+        <el-form-item label="node" :label-width="120">
+          <el-input v-model="dialogForm.node" autocomplete="off"/>
+        </el-form-item>
+        <el-form-item label="model" :label-width="120">
+          <el-input v-model="dialogForm.model" autocomplete="off"/>
+        </el-form-item>
+        <el-form-item label="parameter" :label-width="120">
+          <el-input v-model="dialogForm.parameter" autocomplete="off"/>
+        </el-form-item>
+        <el-form-item label="unit" :label-width="120">
+          <el-input v-model="dialogForm.unit" autocomplete="off"/>
+        </el-form-item>
+        <el-form-item label="range" :label-width="120">
+          <el-input v-model="dialogForm.range" autocomplete="off"/>
+        </el-form-item>
+        <el-form-item label="upload" :label-width="120">
+          <el-input v-model="dialogForm.upload" autocomplete="off"/>
         </el-form-item>
         <el-form-item label="optocouple" :label-width="120">
-          <el-input v-model="dialogForm.optocouple" autocomplete="off" />
+          <el-input v-model="dialogForm.optocouple" autocomplete="off"/>
         </el-form-item>
-        <el-form-item label="Response Level" :label-width="120">
-          <el-input v-model="dialogForm.response_level" autocomplete="off" />
-        </el-form-item>
+
       </el-form>
       <template #footer>
       <span class="dialog-footer">
@@ -77,103 +95,71 @@ import {ElNotification} from "element-plus";
 
 // 数据
 const queryInput = ref("");
-let tableData = ref( [])
+let tableData = ref([])
 // const tableDataCopy = Object.assign(tableData) //此为浅拷贝，并不可靠，为普通类型时可视作深拷贝
 const tableDataCopy = ref([])
 const multipleSelection = ref([])
 const dialogFormVisible = ref(false)
 const dialogForm = ref({
-  name:"zjp",
-  email:"dagwbl@qq.com"
+  'id': null,
+  'node': null,
+  'model': null,
+  'parameter': null,
+  'unit': null,
+  'range': null,
+  'upload': null,
+  'optocouple': null,
 })
+const tableHead = ref([
+  {prop: 'id', label: 'ID'},
+  {prop: 'node', label: 'node'},
+  {prop: 'model', label: 'model'},
+  {prop: 'parameter', label: 'parameter'},
+  {prop: 'unit', label: 'unit'},
+  {prop: 'range', label: 'range'},
+  {prop: 'upload', label: 'upload'},
+  {prop: 'optocouple', label: 'optocouple'},
+])
 const dialogType = ref('add')
 tableData.value = [
   {
-    id:"1",
+    id: "1",
     name: 'Tom1',
-    email:"dabwl@qq.com",
-    phone:"213423451123",
-    state: 'California',
-    address: 'No. 189, Grove St, Los Angeles',
-  },
-  {
-    id:"2",
-    name: 'Tom2',
-    email:"dabwl@qq.com",
-    phone:"213423451123",
-    state: 'California',
-    address: 'No. 189, Grove St, Los Angeles',
-  },
-  {
-    id:"3",
-    name: 'Tom3',
-    email:"dabwl@qq.com",
-    phone:"213423451123",
-    state: 'California',
-    address: 'No. 189, Grove St, Los Angeles',
-  },
-  {
-    id:"4",
-    name: 'Tom4',
-    email:"dabwl@qq.com",
-    phone:"213423451123",
+    email: "dabwl@qq.com",
+    phone: "213423451123",
     state: 'California',
     address: 'No. 189, Grove St, Los Angeles',
   }
 ]
 tableDataCopy.value = [
   {
-    id:"1",
+    id: "1",
     name: 'Tom1',
-    email:"dabwl@qq.com",
-    phone:"213423451123",
-    state: 'California',
-    address: 'No. 189, Grove St, Los Angeles',
-  },
-  {
-    id:"2",
-    name: 'Tom2',
-    email:"dabwl@qq.com",
-    phone:"213423451123",
-    state: 'California',
-    address: 'No. 189, Grove St, Los Angeles',
-  },
-  {
-    id:"3",
-    name: 'Tom3',
-    email:"dabwl@qq.com",
-    phone:"213423451123",
-    state: 'California',
-    address: 'No. 189, Grove St, Los Angeles',
-  },
-  {
-    id:"4",
-    name: 'Tom4',
-    email:"dabwl@qq.com",
-    phone:"213423451123",
+    email: "dabwl@qq.com",
+    phone: "213423451123",
     state: 'California',
     address: 'No. 189, Grove St, Los Angeles',
   }
 ]
 let curPage = ref(1)
 let total = ref(10)
-let pageSize = ref(15)
+let pageSize = ref(20)
 // 方法
 
 // 分页
 
-function handleChangePage(val){
+function handleChangePage(val) {
   getTableData(val)
 }
 
 
 //请求测试,获取表格数据
-async function getTableData(cur=1){
-  let res = await request.get('/projects/learn/ESP/api/node.php?&action=query',{
+async function getTableData(cur = 1) {
+  let res = await request.get('/projects/learn/ESP/api/sensor.php?&action=query', {
     pageSize: pageSize.value,
     p: cur
   });
-  // console.log(res.data.data)
+  console.log(res)
   tableData.value = res.data.data
   total.value = parseInt(res.data.total)
   curPage.value = parseInt(res.data.pageNum)
@@ -181,10 +167,11 @@ async function getTableData(cur=1){
   // return 0
 
 }
+
 getTableData()
 
 // 搜索
-async function handleQueryName(val){
+async function handleQueryName(val) {
   console.log(val.length);
   // if (val.length>0){
   //   tableData.value = tableDataCopy.value.filter(item=>item.name.toLowerCase().match(val));
@@ -193,10 +180,10 @@ async function handleQueryName(val){
   //   tableData.value = tableDataCopy.value;
   //   console.log(tableDataCopy.value);
   // }
-  if (val.length!=0){
-    let res = await request.get("/projects/learn/ESP/api/node.php?&action=query",{coords:val})
+  if (val.length != 0) {
+    let res = await request.get("/projects/learn/ESP/api/sensor.php?&action=query", {coords: val})
     tableData.value = res.data.data
-  }else {
+  } else {
     await getTableData(curPage.value)
 
   }
@@ -206,7 +193,7 @@ async function handleQueryName(val){
 
 // handleQueryName
 // 删除
-async function handleRowDelete(raw){
+async function handleRowDelete(raw) {
 
   // console.log(id);
   // //通过id获取到对应索引
@@ -215,78 +202,79 @@ async function handleRowDelete(raw){
   // //
   // console.log(index);
   // console.log(raw);
-  let res = await request.delete("/projects/learn/ESP/api/node.php?action=delete",{"coords":raw['coords']})
+  let res = await request.delete("/projects/learn/ESP/api/sensor.php?action=delete", {"coords": raw['coords']})
   console.log(res)
-  notify('Success','删除成功','success')
+  notify('Success', '删除成功', 'success')
   await getTableData(curPage.value)
 
 }
 
 //多选
-function handleSelectionChange (val) {
+function handleSelectionChange(val) {
   multipleSelection.value = val;
   // console.log(multipleSelection.value)
   console.log(val);
   multipleSelection.value = []
-  val.forEach(item=>{
+  val.forEach(item => {
     multipleSelection.value.push(item)
   })
   console.log(multipleSelection.value);
 }
 
 // 删除多选
-async function handleDelMulti(){
+async function handleDelMulti() {
   console.log(multipleSelection.value);
-  multipleSelection.value.forEach(item=>{
+  multipleSelection.value.forEach(item => {
     console.log(item);
     handleRowDelete(item)
   })
 
-  notify('Success','删除成功','success')
+  notify('Success', '删除成功', 'success')
   await getTableData(curPage.value)
 
 }
 
 
 //插入
-function handleInsert(){
+function handleInsert() {
   dialogFormVisible.value = true;
   dialogForm.value = {};
 
 }
+
 // 确认
-async function dialogConfirm(){
+async function dialogConfirm() {
   dialogFormVisible.value = false
 
   // 判断是新增还是编辑
-  if (dialogType.value=='add'){
+  if (dialogType.value == 'add') {
     // console.log(dialogForm.value);
     // console.log(tableData.value);
     // //拿到数据添加到tableData中
     // tableData.value.push(dialogForm.value);
     // dialogFormVisible.value = false;
 
-    let res = await request.post("/projects/learn/ESP/api/node.php?action=insert",{...dialogForm.value})
+    let res = await request.post("/projects/learn/ESP/api/sensor.php?action=insert", {...dialogForm.value})
     console.log(res);
     //刷新数据
     await getTableData(curPage.value)
-  }
-  else {
+  } else {
     // 获取当前索引
     // let index = tableData.value.findIndex(item=>item.id === dialogForm.value.id)
     // console.log(index);
     // tableData.value[index]=dialogForm.value;
     // dialogFormVisible.value = false;
-    let res = await request.post("/projects/learn/ESP/api/node.php?action=update",{...dialogForm.value})
+    let res = await request.post("/projects/learn/ESP/api/sensor.php?action=update", {...dialogForm.value})
     console.log(res);
     //刷新数据
     await getTableData(curPage.value)
   }
 
 }
+
 // 编辑
-async function handleEdit(row){
-  dialogFormVisible.value=true;
+async function handleEdit(row) {
+  dialogFormVisible.value = true;
   dialogType.value = 'edit';
   // console.log(row);
   dialogForm.value = {...row}
@@ -294,15 +282,14 @@ async function handleEdit(row){
 
 }
 
-function notify(title,msg,type){
+function notify(title, msg, type) {
   //success, warning, info, error
   ElNotification({
     title: title,
-    message: msg ,
+    message: msg,
     type: type,
   })
 }
-
 
 
 </script>
@@ -311,4 +298,3 @@ function notify(title,msg,type){
 <style scoped>
 @import "../style.css";
 </style>
-
